49657209b5d23abc5814a80260b9cf9319154f56
JarryLi <3244188372@qq.com>
2019-07-16 22:46:12 +0800

    [jerryLi] test:add test for delete company of CompanyController

--- src/test/java/com/tw/apistackbase/controller/CompanyControllerTest.java
+++ src/test/java/com/tw/apistackbase/controller/CompanyControllerTest.java
@@ -1,8 +1,6 @@
 package com.tw.apistackbase.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -73,6 +71,15 @@ public class CompanyControllerTest {
     @Test
     public void should_return_ok_when_call_put_companies_given_new_company() throws Exception {
         mockMvc.perform(put("/companies/1").contentType("application/json;charset=UTF-8").content("{\"companyName\":\"oocl\",\"employees\":[{\"id\":1,\"name\":\"jerry\",\"age\":22,\"gender\":\"male\",\"salary\":10000},{\"id\":2,\"name\":\"laura\",\"age\":22,\"gender\":\"female\",\"salary\":10000}],\"employeesNumber\":0}"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().contentType("text/plain;charset=UTF-8"))
+                .andExpect(content().string(containsString("ok")));
+    }
+
+    @Test
+    public void should_return_ok_when_call_delete_companies_given_1() throws Exception {
+        mockMvc.perform(delete("/companies/1"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().contentType("text/plain;charset=UTF-8"))
