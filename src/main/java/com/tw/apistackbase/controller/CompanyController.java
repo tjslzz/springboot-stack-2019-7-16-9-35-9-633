7b082dbe265707e956da2b0a04f25ec9c8fffdcd
JarryLi <3244188372@qq.com>
2019-07-16 20:59:13 +0800

    [jerryLi] test:add test for get companies of CompanyController

---
+++ src/main/java/com/tw/apistackbase/controller/CompanyController.java
@@ -0,0 +1,20 @@
+package com.tw.apistackbase.controller;
+
+import com.tw.apistackbase.model.Company;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("companies")
+public class CompanyController {
+
+    @GetMapping
+    public List<Company> getCompanies(){
+        return null;
+    }
+
+
+}
---
+++ src/main/java/com/tw/apistackbase/model/Company.java
@@ -0,0 +1,15 @@
+package com.tw.apistackbase.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Company {
+
+
+
+//    public static List<Company> getListForCompanyTest(){
+//        List<Company> companies = new ArrayList<>();
+//        companies.add(new Company());
+//        return companies;
+//    }
+}
